764eb560633ce30314709bd20d9b6b26aa2892d8
mathmaxyz <h.hopecarter@gmail.com>
2026-01-08 15:49:48 +0000

    Refactor to have fading loading animation globally to reduce repeated code

--- frontend/japanese-with-text-fe/src/app/_components/spinner.tsx
+++ frontend/japanese-with-text-fe/src/app/_components/spinner.tsx
@@ -8,7 +8,7 @@ export default function Spinner({ text }: { text: string }) {
 	const height = useSuspenseHeight(text);
 
 	return (
-		<div className="suspense-paragraph" style={{ height: `${height}px` }}>
+		<div className="suspense-paragraph loading" style={{ height: `${height}px` }}>
 		</div>
 	)
 }
