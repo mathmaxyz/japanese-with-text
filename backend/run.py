703b92c1ea9f06fe228e3ae96c980175632a3ed5
mathmaxyz <h.hopecarter@gmail.com>
2026-02-18 18:25:50 +0000

    Implement ctranslate2 for massive translation performance increase

--- backend/run.py
+++ backend/run.py
@@ -3,8 +3,16 @@
 Development server runner for the Language Learning API
 """
 import uvicorn
+from pathlib import Path
+import os
+import sys
 
 if __name__ == "__main__":
+
+    p = Path("./nllb-ct2")
+    if not os.path.exists(p):
+        os.system("ct2-transformers-converter --model facebook/nllb-200-distilled-600M --output_dir nllb-ct2")
+
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
--- backend/translate_service.py
+++ backend/translate_service.py
@@ -1,4 +1,4 @@
-import torch
+import ctranslate2
 from custom_types import TranslateResponse
 import logging
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, BitsAndBytesConfig
@@ -6,24 +6,28 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, BitsAndBytesConfi
 logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s", level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-bnb_config = BitsAndBytesConfig(load_in_8bit=True)
-model = AutoModelForSeq2SeqLM.from_pretrained(
-    "facebook/nllb-200-distilled-600M", 
-    quantization_config=bnb_config,
-    device_map="auto"
-)
+src_lang = "jpn_Jpan"
+tgt_lang = "eng_Latn"
+
+#TODO: Decide on how many inter threads would be required in production
+
 tokenizer = AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M")
+translator = ctranslate2.Translator("nllb-ct2", device="cuda", compute_type="int8", inter_threads=4);
 
 def translate_text(text_chunks: list[str]) -> TranslateResponse | None:
     tokenizer.src_lang = "jpn_Jpan"
+    target_lang_token = tokenizer.convert_ids_to_tokens(
+        tokenizer.convert_tokens_to_ids(tgt_lang)
+    )
     results = []
     for text in text_chunks:
-        inputs = tokenizer(text, return_tensors="pt")
-        inputs = {k: v.to(model.device) for k, v in inputs.items()}
-        translated_tokens = model.generate(
-            **inputs, forced_bos_token_id=tokenizer.convert_tokens_to_ids("eng_Latn"), max_length=1000,
+        source = tokenizer.convert_ids_to_tokens(tokenizer.encode(text)) 
+        output = translator.translate_batch(
+            [source], 
+            target_prefix=[[target_lang_token]],
+            max_decoding_length=1000
         )
-        translation = tokenizer.batch_decode(translated_tokens, skip_special_tokens=True)[0]
+        target = output[0].hypotheses[0]
+        translation = tokenizer.decode(tokenizer.convert_tokens_to_ids(target), skip_special_tokens=True)
         results.append(translation)
     return TranslateResponse(translated_text=results)
-
